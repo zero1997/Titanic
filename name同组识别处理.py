ea32b79355a0d34020fff6d375e9da0b3556e83d
zero1997 <920246199@qq.com>
2018-12-06 18:44:26 +0800

    1

--- name同组识别处理.py
+++ name同组识别处理.py
@@ -18,4 +18,9 @@ all_data['FamilyGroup'] = all_data['Surname'].apply(lambda x: Surname_count[x])
 Female_Child_Group = all_data.loc[(all_data['FamilyGroup'] >= 2) & ((all_data['Age'] <= 12) | (all_data['Sex'] == 'female'))]
 Male_Adult_Group = all_data.loc[(all_data['FamilyGroup'] >= 2) & (all_data['Age'] >= 12) & (all_data['Sex'] == 'male')]
 
-Female_Child_Group = Female_Child_Group.groupby()
+Female_Child_Group = Female_Child_Group.groupby('Surname')['Survived'].mean()
+Dead_List = set(Female_Child_Group[Female_Child_Group.apply(lambda x: x == 0)].index)
+#print Dead_List
+Male_Adult_List = Male_Adult_Group.groupby('Surname')['Survived'].mean()
+Survived_List = set(Male_Adult_List[Male_Adult_List.apply(lambda x: x==1)].index)
+print Survived_List
